7b209851ac91af04f36831fa972d6dbf629f819c
Iagodizlla <iagoschlemper@gmail.com>
2025-08-14 21:39:55 -0300

    mudado namespace de TestBase

--- AcademiaDoZe.Infrastructure.Tests/TestBase.cs
+++ AcademiaDoZe.Infrastructure.Tests/TestBase.cs
@@ -1,31 +1,31 @@
 //Iago Henrique Schlemper
 using AcademiaDoZe.Infrastructure.Data;
-namespace AcademiaDoZe.Infrastructure.Tests
+
+namespace AcademiaDoZe.Infrastructure.Tests;
+
+public abstract class TestBase
 {
-    public abstract class TestBase
+    protected string ConnectionString { get; private set; }
+    protected DatabaseType DatabaseType { get; private set; }
+    protected TestBase()
     {
-        protected string ConnectionString { get; private set; }
-        protected DatabaseType DatabaseType { get; private set; }
-        protected TestBase()
-        {
-            var config = CreateSqlServerConfig();
-            //var config = CreateMySqlConfig();
-            ConnectionString = config.ConnectionString;
-            DatabaseType = config.DatabaseType;
-        }
-        private (string ConnectionString, DatabaseType DatabaseType) CreateSqlServerConfig()
-        {
-            var connectionString = "Server=localhost;Database=db_academia_do_ze;User Id=sa;Password=abcBolinhas12345;TrustServerCertificate=True;Encrypt=True;";
+        var config = CreateSqlServerConfig();
+        //var config = CreateMySqlConfig();
+        ConnectionString = config.ConnectionString;
+        DatabaseType = config.DatabaseType;
+    }
+    private (string ConnectionString, DatabaseType DatabaseType) CreateSqlServerConfig()
+    {
+        var connectionString = "Server=localhost;Database=db_academia_do_ze;User Id=sa;Password=abcBolinhas12345;TrustServerCertificate=True;Encrypt=True;";
 
-            return (connectionString, DatabaseType.SqlServer);
+        return (connectionString, DatabaseType.SqlServer);
 
-        }
-        private (string ConnectionString, DatabaseType DatabaseType) CreateMySqlConfig()
-        {
-            var connectionString = "Server=localhost;Database=db_academia_do_ze;User Id=root;Password=abcBolinhas12345;";
+    }
+    private (string ConnectionString, DatabaseType DatabaseType) CreateMySqlConfig()
+    {
+        var connectionString = "Server=localhost;Database=db_academia_do_ze;User Id=root;Password=abcBolinhas12345;";
 
-            return (connectionString, DatabaseType.MySql);
+        return (connectionString, DatabaseType.MySql);
 
-        }
     }
 }
