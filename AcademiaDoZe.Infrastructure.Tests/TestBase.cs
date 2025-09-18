abc38952fb8d6d1057a2c5eb23ab080cead081d5
iagodizlla <iagoschlemper@gmail.com>
2025-09-17 22:33:22 -0300

    Conexão com o banco feita

--- AcademiaDoZe.Infrastructure.Tests/TestBase.cs
+++ AcademiaDoZe.Infrastructure.Tests/TestBase.cs
@@ -23,7 +23,7 @@ public abstract class TestBase
     //}
     private (string ConnectionString, DatabaseType DatabaseType) CreateMySqlConfig()
     {
-        var connectionString = "Server=localhost;Database=db_academia_do_ze;User Id=root;Password=lilica123@;";
+        var connectionString = "Server=localhost;Port=3306;Database=db_academia_do_ze;Uid=root;Pwd=abcBolinhas12345;";
 
         return (connectionString, DatabaseType.MySql);
 
